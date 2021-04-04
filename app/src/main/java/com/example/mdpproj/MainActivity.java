708ccf4a2cecc7d814fcf558e52587fe9f57d7e9
Hanna <hannabowow@gmail.com>
2021-04-04 17:09:22 -0400

    newlogin

--- app/src/main/java/com/example/mdpproj/MainActivity.java
+++ app/src/main/java/com/example/mdpproj/MainActivity.java
@@ -3,19 +3,43 @@ package com.example.mdpproj;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class
+import com.example.mdpproj.db.AppDatabase;
+import com.example.mdpproj.db.User;
+import com.example.mdpproj.db.UserDao;
 
-MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {
+    TextView usersTextView;
+    EditText firstNameEditText;
+    EditText lastNameEditText;
+    AppDatabase database;
+    UserDao userDao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
+        getSupportActionBar().hide();
         setContentView ( R.layout.activity_main );
-    }
 
+        usersTextView = findViewById(R.id.register);
+        firstNameEditText = findViewById(R.id.username);
+        lastNameEditText = findViewById(R.id.password);
+
+        database = AppDatabase.getDatabase(this);
+        userDao = database.userDao();
+    }
+    public void saveUser(View view) {
+        String firstName = firstNameEditText.getText ( ).toString ( );
+        String lastName = lastNameEditText.getText ( ).toString ( );
+
+        User u = new User ( );
+        u.firstName = firstName;
+        u.lastName = lastName;
+        userDao.insert ( u );
+    }
     public void registerUser(View view) {
         Log.i("mvdev","Hello");
     }
