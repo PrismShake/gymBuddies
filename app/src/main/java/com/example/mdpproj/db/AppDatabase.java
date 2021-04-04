11badedc889893dfd67eb5b3c6d39fbc020b9e7d
Hanna <hannabowow@gmail.com>
2021-04-04 16:39:02 -0400

    revisedlogin with intent

---
+++ app/src/main/java/com/example/mdpproj/db/AppDatabase.java
@@ -0,0 +1,24 @@
+package com.example.mdpproj.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static volatile AppDatabase INSTANCE;
+
+    public static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class, "users")
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return INSTANCE;
+    }
+
+    public abstract UserDao userDao();
+}
